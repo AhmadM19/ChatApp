d4a4674e4fd0b695f81ca5d87b85ab08329a7ac2
AhmadM19 <ahm42@mail.aub.edu>
2023-04-22 02:23:07 +0300

    Added Image Controller and Integration Tests

--- ChatApp.IntegrationTests/BlobImageStoreTests.cs
+++ ChatApp.IntegrationTests/BlobImageStoreTests.cs
@@ -1,12 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure.Core;
+using Azure.Storage.Blobs;
+using ChatApp.Configuration;
+using ChatApp.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace ChatApp.IntegrationTests
 {
-    internal class BlobImageStoreTests
+
+
+    public class BlobImageStoreTest 
     {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly BlobContainerClient _containerClient;
+        private readonly IImageStore _imageStore;
+
+        public BlobImageStoreTest()
+        {
+            // Read the BlobStorage section from the appsettings.Test.json file
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Test.json")
+                .Build();
+            var blobSettings = new BlobSettings();
+            configuration.GetSection("BlobStorage").Bind(blobSettings);
+            // Initialize the BlobServiceClient using the connection string from the configuration
+            _blobServiceClient = new BlobServiceClient(blobSettings.ConnectionString);
+            // Create the test container in the storage account
+            _containerClient = _blobServiceClient.GetBlobContainerClient(blobSettings.ContainerName);
+            // Create a new BlobImageStore object using the test container
+            _imageStore = new BlobImageStore(_blobServiceClient, Options.Create(blobSettings));
+        }
+
+
+        [Fact]
+        public async Task DownloadImage()
+        {
+            var fileName = "avatarProfile-1.jpg";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(),"TestImages",fileName);
+            var file = new FormFile(new FileStream(filePath, FileMode.Open), 0, new FileInfo(filePath).Length, fileName, fileName);
+            using var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+
+            var imageId = await _imageStore.UploadImage(stream.ToArray());
+            var imageData = await _imageStore.DownloadImage(imageId);
+            Assert.NotNull(imageData);
+            await _containerClient.DeleteBlobAsync(imageId);
+        }
+
+        [Fact]
+        public async Task UploadImage()
+        {
+            var fileName = "avatarProfile-2.jpg";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "TestImages", fileName);
+            var file = new FormFile(new FileStream(filePath, FileMode.Open), 0, new FileInfo(filePath).Length, fileName, fileName);
+            using var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+
+            var imageId = await _imageStore.UploadImage(stream.ToArray());
+            Assert.NotNull(imageId);
+            Assert.NotEmpty(imageId);
+            await _containerClient.DeleteBlobAsync(imageId);
+        }
+
+        [Fact]
+        public async Task DeleteImage()
+        {
+            var fileName = "avatarProfile-3.jpg";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "TestImages", fileName);
+            var file = new FormFile(new FileStream(filePath, FileMode.Open), 0, new FileInfo(filePath).Length, fileName, fileName);
+            using var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+
+            var imageId = await _imageStore.UploadImage(stream.ToArray());
+            await _imageStore.DeleteImage(imageId);
+            var blobClient = _containerClient.GetBlobClient(imageId);
+            Assert.False(await blobClient.ExistsAsync());
+        }
+     
+
     }
 }
---
+++ ChatAppTests/Controllers/ImageControllerTest.cs
@@ -0,0 +1,101 @@
+﻿using Azure;
+using ChatApp.Dtos;
+using ChatApp.Storage;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ChatAppTests.Controllers
+{
+    public class ImageControllerTests: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly Mock<IImageStore> _imageStoreMock = new();
+        private readonly HttpClient _httpClient;
+      
+        public ImageControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services => { services.AddSingleton(_imageStoreMock.Object); });
+            }).CreateClient(); 
+        }
+
+        [Fact]
+        public async Task DownloadImage()
+        {
+            var imageFile = File.OpenRead("TestImages/avatarProfile.jpg");
+            var imageStream = new MemoryStream();
+            imageFile.CopyTo(imageStream);
+
+            string imageId = Guid.NewGuid().ToString();
+
+            _imageStoreMock.Setup(m => m.DownloadImage(imageId)).ReturnsAsync(imageStream.ToArray());
+            var response = await _httpClient.GetAsync($"Image/{imageId}");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("image/png", response.Content.Headers.ContentType?.ToString());
+        }
+
+        [Fact]
+        public async Task DownloadImage_NotFound()
+        {
+            string imageId = Guid.NewGuid().ToString();
+            _imageStoreMock.Setup(m => m.DownloadImage(imageId)).ReturnsAsync((byte[]?)null);
+            var response = await _httpClient.GetAsync($"Image/{imageId}");
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task UploadImage()
+        {
+            string str = Guid.NewGuid().ToString();
+            var bytes = Encoding.UTF8.GetBytes(str);
+            var stream = new MemoryStream(bytes);
+
+            HttpContent fileStreamContent = new StreamContent(stream);
+            fileStreamContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "file",
+                FileName = "anything"
+            };
+
+            using var formData = new MultipartFormDataContent();
+            formData.Add(fileStreamContent);
+
+            await _httpClient.PostAsync("/Image", formData);
+
+            _imageStoreMock.Verify(m => m.UploadImage(bytes));
+        }
+
+        [Fact]
+        public async Task DeleteImage()
+        {
+            var imageFile = File.OpenRead("TestImages/avatarProfile.jpg");
+            var imageStream = new MemoryStream();
+            imageFile.CopyTo(imageStream);
+
+            string imageId = Guid.NewGuid().ToString();
+
+            _imageStoreMock.Setup(m => m.DownloadImage(imageId)).ReturnsAsync(imageStream.ToArray());
+
+            var response = await _httpClient.DeleteAsync($"Image/{imageId}");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            _imageStoreMock.Verify(mock => mock.DeleteImage(imageId), Times.Once);
+        }
+
+      
+    }
+}
