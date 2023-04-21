e3b1fcff2635903441442735ab3142b92380e36e
AhmadM19 <ahm42@mail.aub.edu>
2023-04-22 02:15:53 +0300

    Added Profile Service

---
+++ ChatApp/Services/IProfileService.cs
@@ -0,0 +1,18 @@
+﻿using ChatApp.Dtos;
+
+namespace ChatApp.Services
+{
+    public interface IProfileService
+    {
+        /// <exception cref="ProfileNotFoundException">is thrown if the profile does not exists</exception>
+        /// <exception cref="StorageUnavailableException">the database is unavailable</exception>
+        /// <exception cref="DuplicateProfileException">is thrown if the profile already exists</exception>
+        Task UpsertProfile(Profile profile);
+        /// <exception cref="ProfileNotFoundException">is thrown if the profile does not exists</exception>
+        /// <exception cref="StorageUnavailableException">the database is unavailable</exception>
+        Task<Profile?> GetProfile(string username);
+        /// <exception cref="ProfileNotFoundException">is thrown if the profile does not exists</exception>
+        /// <exception cref="StorageUnavailableException">the database is unavailable</exception>
+        Task DeleteProfile(string username);
+    }
+}
---
+++ ChatApp/Services/ProfileService.cs
@@ -0,0 +1,30 @@
+﻿using ChatApp.Dtos;
+using ChatApp.Storage;
+
+namespace ChatApp.Services
+{
+    public class ProfileService: IProfileService
+    {
+        private readonly IProfileStore _profileStore;
+
+        public ProfileService(IProfileStore profileStore)
+        {
+            _profileStore = profileStore;
+        }
+
+        public Task DeleteProfile(string username)
+        {
+            return _profileStore.DeleteProfile(username);
+        }
+
+        public Task<Profile?> GetProfile(string username)
+        {
+            return _profileStore.GetProfile(username);
+        }
+
+        public Task UpsertProfile(Profile profile)
+        {
+            return _profileStore.UpsertProfile(profile);
+        }
+    }
+}
