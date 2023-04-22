6f975313e7fce3f8baa0353abef0a248775b1f20
AhmadM19 <ahm42@mail.aub.edu>
2023-04-22 03:52:01 +0300

    modified program.cs

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -43,11 +43,12 @@ builder.Services.AddAzureClients(clientBuilder =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
