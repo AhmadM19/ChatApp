a1bbeb458bb49160a70594ab85fc6d495a6c9c24
AhmadM19 <ahm42@mail.aub.edu>
2023-04-29 01:01:17 +0300

    Modified ImageService test with exceptions

--- ChatApp/Services/IImageService.cs
+++ ChatApp/Services/IImageService.cs
@@ -1,9 +1,17 @@
-﻿namespace ChatApp.Services
+﻿using ChatApp.Exceptions;
+
+namespace ChatApp.Services
 {
     public interface IImageService
     {
+        /// <exception cref="ArgumentNullException">if uploaded image is null</exception>
+        /// <exception cref="StorageUnavailableException">if the database is unavailable</exception>
         Task<string> UploadImage(byte[] file);
+        /// <exception cref="ImageNotFoundException"> if the image does not exists</exception>
+        /// <exception cref="StorageUnavailableException">if the database is unavailable</exception>
         Task<byte[]> DownloadImage(string id);
+        /// <exception cref="ImageNotFoundException"> if the image does not exists</exception>
+        /// <exception cref="StorageUnavailableException">if the database is unavailable</exception>
         Task DeleteImage(string id);
     }
 }
