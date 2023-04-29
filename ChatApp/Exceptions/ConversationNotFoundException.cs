d891bdf7ef047f70ab548d2c733425a8a1266cec
AhmadM19 <ahm42@mail.aub.edu>
2023-04-29 22:18:51 +0300

    Added New Exceptions

---
+++ ChatApp/Exceptions/ConversationNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Exceptions
+{
+    public class ConversationNotFoundException : Exception
+    {
+        public ConversationNotFoundException(string? conversationId)
+            : base($"conversation with id {conversationId} doesn't exist")
+        {
+        }
+    }
+}
